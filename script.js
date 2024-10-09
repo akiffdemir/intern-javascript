4631f9d810c4de3dc5bb563da8c47696e40c4eef
akiffdemir <117564534+akiffdemir@users.noreply.github.com>
2024-10-09 16:13:03 +0300

    intern

---
+++ script.js
@@ -0,0 +1,200 @@
+console.log("merhaba");
+try{
+console.log(1234);
+console.log(true);
+console.log([1,2,3,4]);
+console.error("hata oluştu");
+console.warn("bir uyarı oluştu")
+} catch (error) {
+    console.error(error)
+}
+// tek satırlık yorum satırı
+
+/* 
+
+ yorum satırları
+  yorum satırları
+
+   yorum satırları
+*/
+
+console.clear();
+
+
+// Değişkenler
+
+// var age;
+// console.log(age)
+
+// age=20
+// console.log(age);
+
+// var fullname="Akif Demir";
+// console.log(fullname)
+
+// var fullname="Mehmet Demir";
+// console.log(fullname) 
+
+// // değişken tanımlama kuralalrı
+
+// //sayısal ifade başlayamaz
+//  var yas1;
+//  var _yas2;
+//  var $yas3;
+
+//  //komut isimleriyle tanımlama yapılamaz.
+//  // Birden fazla kelime
+
+//  var ad='akif';
+//  var soyad='demir';
+ 
+//  var ad_soyad='akif demir';
+//   var adSoyad='mehmet demir';
+//   // case sensitive (büyük küçük duyarlılığı)
+
+//   var firstName='mehmet';
+//   var FirstName='akif';
+
+//   console.log(firstName);
+//   console.log(FirstName);
+
+
+//   // var , let , const
+
+//   let city='antalya';
+//   console.log(city);
+
+//   const email='abc@gmail.com'
+//   console.log(email);
+
+// Ders 3 : Değişken Tipleri
+
+//Pritive Types
+
+// String
+// let firstName='Demir';
+// console.log(typeof firstName);
+
+// //Number
+// let age = 22;
+// let money = 100.5;
+
+// //Boolean
+// let isActive = false
+
+// //null
+// let job=null;
+
+// //undefined
+// let car;
+
+
+
+
+
+// // Reference Types - Objects
+
+// //Array
+
+// let names=['Ali','ahmet','Can'];
+
+// //Object
+
+// let addres = {
+//     city:'Antalya',
+//     country:'Türkiye'
+// }
+
+// //Function
+
+// var calculateAge = function(){
+//     return;0;
+// };
+
+// console.log(typeof calculateAge);
+
+// Ders 4 : Tür Dçnüşümü
+
+// number to string
+
+// val = String(10);
+
+// // bool to string
+// val = String(true);
+
+// //date to string
+// val = String(new Date());
+
+// //array to string
+// val = String([1,2,3,4])
+
+// // toString()
+// val = (10).toString();
+// val = (false).toString();
+
+// // String to Number
+
+// val = Number('10');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('a');
+// val = Number([1,2,3,4]);
+
+// // parseInt
+// // parseFloat
+
+// val = parseInt('10');
+// val = parseInt('10.5');
+// val = parseFloat('10.5');
+
+
+
+
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.toFixed(2));
+
+
+// ** Bir müşterinin aşağıdaki bilgileri için
+//  değişken oluşturunuz. 
+
+// Müşteri adı
+// Müşteri soyadı
+// Müşteri tc kimlik
+// Müşteri sipariş toplamı
+// Müşteri cinsiyet
+// Müşteri adres bilgisi
+// Müşteri hobiler
+
+// Aşağıdaki sipraişlerin toplamını hesaplayınız.
+
+var order1 =Number('100');
+var order2 =Number('150');
+
+var toplam = (order1+order2)
+console.log(toplam);
+
+
+// Aşağıdaki sipraişlerin toplamını hesaplayınız.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+ 
